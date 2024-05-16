0030dd9b373ce16574c1f062a55ab982b582ff17
SantiagoMurilloV <128425308+SantiagoMurilloV@users.noreply.github.com>
2024-05-16 10:01:58 -0500

    firt commit, and create a table of display data

---
+++ src/components/TableComponent.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import './style/Table.css'
+const TableComponent = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('https://pai-project-1-831bcc92ff5d.herokuapp.com/api/data');
+            const jsonData = await response.json();
+            jsonData.forEach(item => {
+                const [dateString, timeString] = item.actualDate.split(', ');
+                const [day, month, year] = dateString.split('/').map(part => parseInt(part, 10));
+                const [time, period] = timeString.split(' ');
+                let [hours, minutes] = time.split(':').map(part => parseInt(part, 10));
+                if (period === 'p. m.' && hours !== 12) {
+                    hours += 12;
+                } else if (period === 'a. m.' && hours === 12) {
+                    hours = 0;
+                }
+                item.parsedDate = new Date(year, month - 1, day, hours, minutes);
+            });
+
+            jsonData.sort((a, b) => {
+                if (a.parsedDate.getTime() === b.parsedDate.getTime()) {
+                    return a.parsedDate.getHours() - b.parsedDate.getHours();
+                }
+                return b.parsedDate.getTime() - a.parsedDate.getTime();
+            });
+
+            setData(jsonData);
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Tabla de Datos Obtenidos por Raspberry pi Zero 2W</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Fecha/Hora</th>
+                        <th>mH</th>
+                        <th>Pho_7in1</th>
+                        <th>Pot_7in1</th>
+                        <th>Nit_7in1</th>
+                        <th>Moi_7in1</th>
+                        <th>Tem_7in1</th>
+                        <th>Ph_7in1</th>
+                        <th>Con_7in1</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(item => (
+                        <tr key={item._id}>
+                            <td>{item.actualDate}</td>
+                            <td>{item.mH}</td>
+                            <td>{item.Pho_7in1}</td>
+                            <td>{item.Pot_7in1}</td>
+                            <td>{item.Nit_7in1}</td>
+                            <td>{item.Moi_7in1}</td>
+                            <td>{item.Tem_7in1}</td>
+                            <td>{item.Ph_7in1}</td>
+                            <td>{item.Con_7in1}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default TableComponent;
